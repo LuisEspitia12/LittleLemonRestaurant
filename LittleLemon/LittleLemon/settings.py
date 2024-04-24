4d00ea46774cdf65636d33d681a2abf5bf2c9422
LuisEspitia12 <luis.espitia.rodriguez@gmail.com>
2024-04-23 21:16:09 -0500

    add static files like the logo, modify index.htmland update settings.py

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# The settings for static files have been updated for the Graded assessment
+STATIC_URL = 'restaurant/static/'
+
+STATICFILES_DIRS = [
+    "restaurant/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
