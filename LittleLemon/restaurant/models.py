338e56221083f761f282ab32acdc5233d1e2d84e
LuisEspitia12 <luis.espitia.rodriguez@gmail.com>
2024-04-25 11:30:45 -0500

    add models of booking and  menu also create the super user lucho

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    BookingDate = models.DateField()
+    No_of_guests = models.SmallIntegerField(default=2)
+
+    def __str__(self): 
+        return self.Name
+
+
+# Add code to create Menu model
+class Menu(models.Model):
+   title = models.CharField(max_length=255) 
+   price = models.DecimalField(max_digits=10, decimal_places=2)   
+   inventory = models.IntegerField(default=5)
+
+   def __str__(self):
+      return self.title
