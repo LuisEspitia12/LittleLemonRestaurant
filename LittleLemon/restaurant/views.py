ddefe8b5ec847e10649101564ffaabaf32bf7da5
LuisEspitia12 <luis.espitia.rodriguez@gmail.com>
2024-05-02 08:46:22 -0500

    Booking table api
    -Edit  setting.py to add  'rest_framework' in Installed_apps
    
    -Edit urls.py to  add router 'table'.
    
    -Create serializers.py  to identify the booking table and finally edit  view.py  for MenuItemsView , singleMenuItemsView and BookingViewSet

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()  # Fetch all objects from the Booking model
+    serializer_class = BookingSerializer  # Set the serializer class to BookingSerializer
+
+#class UserViewSet(viewsets.ModelViewSet):
+#   queryset = User.objects.all() 
+#   serializer_class = UserSerializer
+#   permission_classes = [permissions.IsAuthenticated] 
